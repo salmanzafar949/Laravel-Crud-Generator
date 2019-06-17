6858d51f0b5fcb63d0390cda039ace7af910daca
Salman Zafar <salman.zafar@innowi.com>
2019-06-17 16:12:33 +0500

    defined structure of package

---
+++ src/CrudGeneratorClass/CrudGeneratorService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Salman\CrudGenerator\CrudGeneratorClass;
+
+class CrudGeneratorService
+{
+
+    /**
+     * @param $name
+     * This will create controller from stub file
+     */
+    public static function MakeController($name)
+    {
+
+    }
+
+    /**
+     * @param $name
+     * This will create model from stub file
+     */
+    public static function MakeModel($name)
+    {
+
+    }
+
+    /**
+     * @param $name
+     * This will create Request from stub file
+     */
+    public static function MakeRequest($name)
+    {
+
+    }
+
+    /**
+     * @param $name
+     * This will create migration using artisan command
+     */
+    public static function MakeMigration($name)
+    {
+
+    }
+}
