1446237b6fd832cc35b0c730bca3291154bd28e4
Salman Zafar <salman.zafar@innowi.com>
2019-06-17 16:24:10 +0500

    added create model controller migration feature

--- src/CrudGeneratorClass/CrudGeneratorService.php
+++ src/CrudGeneratorClass/CrudGeneratorService.php
@@ -2,8 +2,17 @@
 
 namespace Salman\CrudGenerator\CrudGeneratorClass;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+
 class CrudGeneratorService
 {
+
+    protected static function GetStubs($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
 
     /**
      * @param $name
@@ -11,6 +20,22 @@ class CrudGeneratorService
      */
     public static function MakeController($name)
     {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+            ],
+            [
+                $name,
+                strtolower( Str::plural($name)),
+                strtolower($name)
+            ],
+
+           CrudGeneratorService::GetStubs('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $template);
 
     }
 
@@ -20,7 +45,13 @@ class CrudGeneratorService
      */
     public static function MakeModel($name)
     {
+        $template = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            CrudGeneratorService::GetStubs('Model')
+        );
 
+        file_put_contents(app_path("/{$name}.php"), $template);
     }
 
     /**
@@ -29,7 +60,16 @@ class CrudGeneratorService
      */
     public static function MakeRequest($name)
     {
+        $template = str_replace(
+            ['{{modelName}}'],
+            [$name],
+           CrudGeneratorService::GetStubs('Request')
+        );
 
+        if (!file_exists($path=app_path('/Http/Requests/')))
+            mkdir($path, 0777, true);
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $template);
     }
 
     /**
@@ -38,6 +78,7 @@ class CrudGeneratorService
      */
     public static function MakeMigration($name)
     {
+        Artisan::call('make:migration create_'. strtolower( Str::plural($name)).'_table --create='. strtolower( Str::plural($name)));
 
     }
 }
